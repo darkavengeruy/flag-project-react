ffcbc099b16560cdb9829e07c5ed87609f3a023e
FERNANDO FRANCO <ferfranco@hotmail.com>
2020-06-09 03:04:32 -0400

    Updates

---
+++ precache-manifest.a1843b62795fbd2071caa831d306f846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00fcb7e4f60d068ed28ec00aefa19ea",
+    "url": "/darkavengeruy/flag-project-react/index.html"
+  },
+  {
+    "revision": "0355da9b279d52b5d0c7",
+    "url": "/darkavengeruy/flag-project-react/static/css/main.fcd830c8.chunk.css"
+  },
+  {
+    "revision": "4a0b3d0542adbdea8291",
+    "url": "/darkavengeruy/flag-project-react/static/js/2.4c65cd31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/darkavengeruy/flag-project-react/static/js/2.4c65cd31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0355da9b279d52b5d0c7",
+    "url": "/darkavengeruy/flag-project-react/static/js/main.eeef285f.chunk.js"
+  },
+  {
+    "revision": "255aa9833c0c866d6ee6",
+    "url": "/darkavengeruy/flag-project-react/static/js/runtime-main.603c3f3d.js"
+  }
+]);
